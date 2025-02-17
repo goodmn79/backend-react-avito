beab33820722b5f4df436ccb90b1b904e3a5b99f
barkhudaryan-arman <armanik147@gmail.com>
2025-02-18 00:19:46 +0300

    #39 создал бд avito_db и переменные окружения для подключения к ней, создал интерфейс CommentRepository

--- src/main/java/ru/skypro/homework/dto/comment/CommentEntity.java
+++ src/main/java/ru/skypro/homework/dto/comment/CommentEntity.java
@@ -1,14 +1,12 @@
 package ru.skypro.homework.dto.comment;
 
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "comments")
 public class CommentEntity {
