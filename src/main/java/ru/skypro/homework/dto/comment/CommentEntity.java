546f5b5a846ee7b5fcec91601eb4df0db0650175
barkhudaryan-arman <armanik147@gmail.com>
2025-02-19 09:07:10 +0300

    #36 сделал изменения в классе CommentEntity и Comment
    # Conflicts:
    #	src/main/java/ru/skypro/homework/dto/comment/Comment.java

--- src/main/java/ru/skypro/homework/dto/comment/CommentEntity.java
+++ src/main/java/ru/skypro/homework/dto/comment/CommentEntity.java
@@ -1,23 +1,17 @@
 package ru.skypro.homework.dto.comment;
 
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "comments")
 public class CommentEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int pk;
-    private long author;
-    private String authorImage;
-    private String authorFirstName;
-    private int cratedAt;
+    private long pk;
+    private long cratedAt;
     private String text;
 }
