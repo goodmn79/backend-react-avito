7fb4cd4945a594a8d69f0300b0f95ecdd7510968
JuliaCherepnev <yuliya.cherepnev@mail.ru>
2025-02-19 14:48:19 +0400

    #54 изменила название строки

--- src/main/java/ru/skypro/homework/validate/DataValidator.java
+++ src/main/java/ru/skypro/homework/validate/DataValidator.java
@@ -8,19 +8,19 @@ import java.util.regex.Pattern;
 
 @Component
 public class DataValidator {
-    public void validateStringLengthRange (String str, int maxLength, int minLength) throws IllegalDataException {
-        if (str.length() < minLength || str.length() > maxLength) {
+    public void validateStringLengthRange(String input, int maxLength, int minLength) {
+        if (input.length() < minLength || input.length() > maxLength) {
             throw new IllegalDataException("Длина строки должна быть от " + minLength + " до " + maxLength + " символов.");
         }
     }
 
-    public void validatePrice (int price) throws IllegalDataException {
+    public void validatePrice(int price) {
         if (price < 0 || price > 10_000_000) {
             throw new IllegalDataException("Цена должна быть от 0 до 10 000 000.");
         }
     }
 
-    public void validatePhone (String phone) throws IllegalDataException {
+    public void validatePhone(String phone) {
         String pattern = "^\\+7\\s?\\(?\\d{3}\\)?\\s?\\d{3}-?\\d{2}-?\\d{2}$";
 
         Pattern compiledPattern = Pattern.compile(pattern);
@@ -30,8 +30,6 @@ public class DataValidator {
             throw new IllegalDataException("Номер телефона должен соответствовать формату: +7 (XXX) XXX-XX-XX");
         }
     }
-
-
 
 
 }
