97d2e62fd17ee25eb079365f4af8921aa7454ebb
JuliaCherepnev <yuliya.cherepnev@mail.ru>
2025-02-19 16:03:47 +0400

    #41 Добавила валидацию данных

--- src/main/java/ru/skypro/homework/mapper/AdMapper.java
+++ src/main/java/ru/skypro/homework/mapper/AdMapper.java
@@ -1,81 +1,40 @@
 package ru.skypro.homework.mapper;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import ru.skypro.homework.dto.ad.Ad;
+import ru.skypro.homework.validate.DataValidator;
 
+@Component
+@RequiredArgsConstructor
 public class AdMapper {
+    private final DataValidator validator;
+
     // Преобразование из AdEntity в Ad (DTO)
-    public toAd(AdEntity adEntity) {
-        if (adEntity == null) {
-            throw new IllegalArgumentException("Объявление не может быть null");
-        }
+    public Ad toAd(AdEntity adEntity) {
         Ad ad = new Ad();
-
-        if (adEntity.getPk() <= 0) {
-            throw new IllegalArgumentException("Некорректный ID объявления");
-        }
-        ad.setPk(adEntity.getPk());
-
-        if (adEntity.getImage() == null || adEntity.getImage().isEmpty()) {
-            throw new IllegalArgumentException("Изображение не может быть пустым");
-        }
-        ad.setImage(adEntity.getImage());
-
-        if (adEntity.getPrice() < 0 || adEntity.getPrice() > 10000000) {
-            throw new IllegalArgumentException("Цена должна быть в диапазоне от 0 до 10,000,000");
-        }
-        ad.setPrice(adEntity.getPrice());
-
-        if (adEntity.getTitle() == null || adEntity.getTitle().length() < 4 || adEntity.getTitle().length() > 32) {
-            throw new IllegalArgumentException("Заголовок должен содержать от 4 до 32 символов");
-        }
-        ad.setTitle(adEntity.getTitle());
-
-        if (adEntity.getAuthor() == null || adEntity.getAuthor() <= 0) {
-            throw new IllegalArgumentException("Автор должен быть задан корректным ID");
-        }
-        ad.setAuthor(adEntity.getAuthor());
-        return ad;
+        return ad.setPk(adEntity.getPk())
+                .setImage(adEntity.getImage())
+                .setPrice(adEntity.getPrice())
+                .setTitle(adEntity.getTitle())
+                .setAuthor(adEntity.getAuthor());
     }
 
     // Преобразование из Ad (DTO) в AdEntity
-    public toAdEntity(Ad ad) {
-        if (ad == null) {
-            throw new IllegalArgumentException("Объявление не может быть null");
-        }
+    public AdEntity toAdEntity(Ad ad) {
 
-        AdEntity adEntity = new AdEntity();
-        if (ad.getImage() == null || ad.getImage().isEmpty()) {
-            throw new IllegalArgumentException("Изображение не может быть пустым");
-        }
-        adEntity.setImage(ad.getImage());
+        validator.validateStringLengthRange(32, 4, ad.getTitle());
+        validator.validateStringLengthRange(64, 8, ad.getDescription());
+        validator.validatePrice(ad.getPrice());
+        validator.validatePhone(ad.getPhone());
 
-        if (ad.getPk() <= 0) {
-            throw new IllegalArgumentException("Некорректный ID объявления");
-        }
-        adEntity.setPk(ad.getPk());
-
-        if (ad.getPrice() < 0 || ad.getPrice() > 10000000) {
-            throw new IllegalArgumentException("Цена должна быть в диапазоне от 0 до 10,000,000");
-        }
-        adEntity.setPrice(ad.getPrice());
-
-        if (ad.getTitle() == null || ad.getTitle().length() < 4 || ad.getTitle().length() > 32) {
-            throw new IllegalArgumentException("Заголовок должен содержать от 4 до 32 символов");
-        }
-        adEntity.setTitle(ad.getTitle());
-
-        if (ad.getDescription() != null && (ad.getDescription().length() < 8 || ad.getDescription().length() > 64)) {
-            throw new IllegalArgumentException("Описание должно содержать от 8 до 64 символов");
-        }
-        adEntity.setDescription(ad.getDescription());
-
-        if (ad.getAuthor() <= 0) {
-            throw new IllegalArgumentException("Автор объявления должен быть задан корректным ID");
-        }
-        UserEntity author = new UserEntity();
-        author.setId(ad.getAuthor());
-        adEntity.setAuthor(author);
-
-        return adEntity;
+        return new AdEntity()
+                .setImage(ad.getImage())
+                .setPk(ad.getPk())
+                .setPrice(ad.getPrice())
+                .setTitle(ad.getTitle())
+                .setDescription(ad.getDescription())
+                .setAuthor(new UserEntity().setId(ad.getAuthor()));
     }
+
 }
