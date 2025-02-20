8fe41c81e51e4962b193711c4f8f1299e376693e
goodmn79 <goodmn79@gmail.com>
2025-02-20 13:24:09 +0300

    #34-UserEntity создал классы сущностей UserEntity и Image

---
+++ src/main/java/ru/skypro/homework/model/Image.java
@@ -0,0 +1,25 @@
+package ru.skypro.homework.model;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "images")
+@Data
+@Accessors(chain = true)
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String path;
+    private long size;
+    private String mediaType;
+    @Transient
+    private byte[] data;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+}
