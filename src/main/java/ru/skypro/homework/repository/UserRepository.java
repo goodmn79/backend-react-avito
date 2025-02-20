333430274fe6a96a7bbb2dd795ea29454ad8c56b
goodmn79 <goodmn79@gmail.com>
2025-02-20 13:26:19 +0300

    #40-UserRepo создал классы репозиториев UserRepository и ImageRepository

---
+++ src/main/java/ru/skypro/homework/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package ru.skypro.homework.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.skypro.homework.model.UserEntity;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+}
