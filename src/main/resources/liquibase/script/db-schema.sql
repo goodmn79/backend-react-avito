0e3bb5d9941d760d084093588fc6c38e93a932da
goodmn79 <goodmn79@gmail.com>
2025-02-20 13:08:37 +0300

    #37 добавил создание схем images и users в файл db-schema.sql

--- src/main/resources/liquibase/script/db-schema.sql
+++ src/main/resources/liquibase/script/db-schema.sql
@@ -1,1 +1,24 @@
 --liquibase formatted sql
+
+--changeset goodmn:1
+
+CREATE TABLE images
+(
+    id        BIGINT PRIMARY KEY,
+    path      VARCHAR(32),
+    size      BIGINT,
+    media_type VARCHAR(128)
+);
+
+CREATE TABLE users
+(
+    id         BIGINT PRIMARY KEY,
+    username   VARCHAR(32),
+    password   VARCHAR(16),
+    email      VARCHAR(128),
+    first_name VARCHAR(16),
+    last_name  VARCHAR(16),
+    phone      VARCHAR(32),
+    role       VARCHAR(8),
+    image_id   BIGINT REFERENCES images (id)
+);
