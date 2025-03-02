e363b88b0e8af5a3a03fb07e8b431a3ef7baca8c
goodmn79 <goodmn79@gmail.com>
2025-03-02 17:50:26 +0300

    add_ad_service изменил тип данных для id и пароля в бд

--- src/main/resources/script/changesets.sql
+++ src/main/resources/script/changesets.sql
@@ -4,7 +4,7 @@
 
 CREATE TABLE IF NOT EXISTS images
 (
-    id         INT PRIMARY KEY,
+    id         SERIAL PRIMARY KEY,
     path       VARCHAR(32)  NOT NULL,
     size       BIGINT       NOT NULL,
     media_type VARCHAR(128) NOT NULL
@@ -12,9 +12,9 @@ CREATE TABLE IF NOT EXISTS images
 
 CREATE TABLE IF NOT EXISTS users
 (
-    id         INT PRIMARY KEY,
+    id         SERIAL PRIMARY KEY,
     username   VARCHAR(32) UNIQUE NOT NULL,
-    password   VARCHAR(16)        NOT NULL,
+    password   TEXT               NOT NULL,
     email      VARCHAR(128),
     first_name VARCHAR(16)        NOT NULL,
     last_name  VARCHAR(16)        NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS users
 
 CREATE TABLE IF NOT EXISTS ads
 (
-    pk          INT PRIMARY KEY,
+    pk          SERIAL PRIMARY KEY,
     price       INT         NOT NULL,
     title       VARCHAR(32) NOT NULL,
     description VARCHAR(64),
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS ads
 
 CREATE TABLE IF NOT EXISTS comments
 (
-    pk        INT PRIMARY KEY,
+    pk        SERIAL PRIMARY KEY,
     crated_at BIGINT      NOT NULL,
     text      VARCHAR(32) NOT NULL,
     author_id INT REFERENCES users (id),
