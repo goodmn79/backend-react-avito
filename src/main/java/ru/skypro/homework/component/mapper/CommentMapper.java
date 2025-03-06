82f6904bd302240c175cfc08459c8b6a08ca20ad
JuliaCherepnev <yuliya.cherepnev@mail.ru>
2025-03-06 18:45:59 +0400

    #78 Добавила комментарии для мапперов

--- src/main/java/ru/skypro/homework/component/mapper/CommentMapper.java
+++ src/main/java/ru/skypro/homework/component/mapper/CommentMapper.java
@@ -12,11 +12,24 @@ import ru.skypro.homework.service.UserService;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
+/**
+ * Маппер для преобразования комментариев к объявлениям между DTO и сущностями.
+ *
+ * @author Powered by ©AYE.team
+ * @version 0.0.1-SNAPSHOT
+ */
 @Component
 @RequiredArgsConstructor
 public class CommentMapper {
     private final UserService userService;
 
+    /**
+     * Преобразует {@link CommentEntity} в {@link Comment}.
+     *
+     * @param commentEntity Сущность, которую нужно преобразовать
+     * @return {@link Comment} DTO содержащий комментарий к объявлению и дополняющую его информацию
+     */
     public Comment map(CommentEntity commentEntity) {
         String imagePath = commentEntity.getAuthor().getImage() == null ? null : commentEntity.getAuthor().getImage().getPath();
         return new Comment()
@@ -28,6 +41,12 @@ public class CommentMapper {
                 .setText(commentEntity.getText());
     }
 
+    /**
+     * Преобразует {@link CreateOrUpdateComment} в {@link CommentEntity}.
+     *
+     * @param comment DTO, который нужно преобразовать
+     * @return {@link CommentEntity}, сущность содержащая комментарий к объявлению
+     */
     public CommentEntity map(CreateOrUpdateComment comment) {
         return new CommentEntity()
                 .setAuthor(userService.getCurrentUser())
@@ -35,6 +54,12 @@ public class CommentMapper {
                 .setText(DataValidator.validatedData(comment.getText(), 8, 64));
     }
 
+    /**
+     * Преобразует список {@link CommentEntity} в {@link Comments}.
+     *
+     * @param comments Список комментариев, который нужно преобразовать
+     * @return {@link Comments}, объект DTO содержащий информацию об общем количестве комментариев и их список
+     */
     public Comments map(List<CommentEntity> comments) {
         return new Comments()
                 .setCount(comments.size())
