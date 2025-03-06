47a94d183a6fdccb2dc66335a74079edd89e078e
JuliaCherepnev <yuliya.cherepnev@mail.ru>
2025-03-06 15:35:55 +0400

    #74 Реализация dip

--- src/main/java/ru/skypro/homework/service/UserService.java
+++ src/main/java/ru/skypro/homework/service/UserService.java
@@ -1,102 +1,25 @@
 package ru.skypro.homework.service;
 
-import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import ru.skypro.homework.component.mapper.UserMapper;
-import ru.skypro.homework.component.validation.DataValidator;
 import ru.skypro.homework.dto.user.NewPassword;
 import ru.skypro.homework.dto.user.Register;
 import ru.skypro.homework.dto.user.UpdateUser;
 import ru.skypro.homework.dto.user.User;
-import ru.skypro.homework.entity.Image;
 import ru.skypro.homework.entity.UserEntity;
-import ru.skypro.homework.exception.UserNotFoundException;
-import ru.skypro.homework.exception.WrongCurrentPasswordException;
-import ru.skypro.homework.repository.UserRepository;
-
 import java.io.IOException;
 
-@Service
-@RequiredArgsConstructor
-public class UserService {
-    private final PasswordEncoder encoder;
-    private final UserRepository userRepository;
-    private final UserMapper userMapper;
-    private final ImageService imageService;
+public interface UserService {
+    void updatePassword(NewPassword newPassword);
 
-    private final Logger log = LoggerFactory.getLogger(UserService.class);
+    User getUser();
 
-    public void updatePassword(NewPassword newPassword) {
-        log.warn("Изменение пароля авторизованного пользователя.");
-        String currentPassword =
-                DataValidator.validatedData(newPassword.getCurrentPassword(), 8, 16);
-        this.checkPassword(currentPassword);
-        String updatedPassword =
-                encoder.encode(
-                        DataValidator.validatedData(newPassword.getNewPassword(), 8, 16)
-                );
-        userRepository.save(this.getCurrentUser().setPassword(updatedPassword));
-        log.info("Пароль авторизованного пользователя успешно изменён.");
-    }
+    UpdateUser updateUser(UpdateUser updateUser);
 
-    public User getUser() {
-        log.warn("Получение данных авторизованного пользователя.");
-        User user = userMapper.map(this.getCurrentUser());
-        log.info("Данные авторизованного пользователя успешно получены.");
-        return user;
-    }
+    void addUser(Register register);
 
-    public UpdateUser updateUser(UpdateUser updateUser) {
-        log.warn("Обновление данных авторизованного пользователя.");
-        UserEntity user =
-                this.getCurrentUser()
-                        .setFirstName(
-                                DataValidator.validatedData(updateUser.getFirstName(), 2, 16))
-                        .setLastName(
-                                DataValidator.validatedData(updateUser.getLastName(), 2, 16))
-                        .setPhone(
-                                DataValidator.validatedPhoneNumber(updateUser.getPhone()));
-        userRepository.save(user);
-        log.info("Данные авторизованного пользователя успешно обновлены.");
-        return updateUser;
-    }
+    void updateUserImage(MultipartFile file) throws IOException;
 
-    public void addUser(Register register) {
-        log.warn("Валидация данных нового пользователя.");
-        UserEntity user = userMapper.map(register);
-        log.warn("Сохранение нового пользователя в базе данных.");
-        userRepository.save(user);
-        log.info("Пользователь успешно сохранён.");
-    }
+    boolean userExists(String username);
 
-    public void updateUserImage(MultipartFile file) throws IOException {
-        log.warn("Обновление аватара текущего пользователя.");
-        UserEntity user = this.getCurrentUser();
-        Image userImage = imageService.saveImage(file, user.getId());
-        userRepository.save(user.setImage(userImage));
-        log.info("Аватар успешно обновлён.");
-    }
-
-    public boolean userExists(String username) {
-        boolean isExists = userRepository.existsByUsername(username);
-        log.debug("Проверка существования текущего пользоввателя, результат: '{}'", isExists);
-        return isExists;
-    }
-
-    public UserEntity getCurrentUser() {
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        return userRepository.findByUsername(username)
-                .orElseThrow(UserNotFoundException::new);
-    }
-
-    private void checkPassword(String password) {
-        if (!encoder.matches(password, this.getCurrentUser().getPassword())) {
-            throw new WrongCurrentPasswordException("The wrong current password has been introduced");
-        }
-    }
+    UserEntity getCurrentUser();
 }
---
+++ src/main/java/ru/skypro/homework/service/impl/ImageServiceImpl.java
@@ -0,0 +1,69 @@
+package ru.skypro.homework.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import ru.skypro.homework.component.validation.DataValidator;
+import ru.skypro.homework.entity.Image;
+import ru.skypro.homework.enums.ImageExtension;
+import ru.skypro.homework.exception.ImageNotFoundException;
+import ru.skypro.homework.repository.ImageRepository;
+import ru.skypro.homework.service.ImageService;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Service
+@RequiredArgsConstructor
+public class ImageServiceImpl implements ImageService {
+    @Value("${application.images.directory}")
+    private String imageDir;
+
+    private final ImageRepository imageRepository;
+
+    private final Logger log = LoggerFactory.getLogger(ImageServiceImpl.class);
+
+    @Override
+    public Image saveImage(MultipartFile file, int id) throws IOException {
+        log.info("Сохранение фото.");
+        Image image = imageRepository.findById(id).orElse(new Image());
+        String path = image.getPath() == null ? this.buildFileName(file) : image.getPath();
+        image.setPath(path)
+                .setSize(file.getSize())
+                .setMediaType(file.getContentType())
+                .setData(file.getBytes());
+        this.saveToDir(image);
+        return imageRepository.save(image);
+    }
+
+    @Override
+    public void removeImage(int imageId) {
+        try {
+            Image image =
+                    imageRepository.findById(imageId)
+                            .orElseThrow(ImageNotFoundException::new);
+            String path = imageDir + image.getPath();
+            Files.deleteIfExists(Path.of(path));
+            imageRepository.delete(image);
+        } catch (Exception e) {
+            log.error("Изображение не найдено!");
+        }
+    }
+
+    private void saveToDir(Image image) throws IOException {
+        String path = imageDir + image.getPath();
+        Path imagePath = Path.of(path);
+        Files.write(imagePath, image.getData());
+        log.debug("Изображение сохранено в: '{}'", path);
+    }
+
+    @Override
+    public String buildFileName(MultipartFile file) {
+        String fileName = DataValidator.validatedImage(file);
+        return System.currentTimeMillis() + ImageExtension.getExtension(fileName);
+    }
+}
