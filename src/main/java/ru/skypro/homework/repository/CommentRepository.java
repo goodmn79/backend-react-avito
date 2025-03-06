c546c4a7b991174d1720b9e9c55a77dc72dda415
JuliaCherepnev <yuliya.cherepnev@mail.ru>
2025-03-06 23:22:54 +0400

    #78 Добавила комментарии для репозиториев

--- src/main/java/ru/skypro/homework/repository/CommentRepository.java
+++ src/main/java/ru/skypro/homework/repository/CommentRepository.java
@@ -6,7 +6,22 @@ import ru.skypro.homework.entity.CommentEntity;
 
 import java.util.List;
 
+/**
+ * Репозиторий для работы с таблицей COMMENTS в базе данных.
+ * <p>
+ * Этот класс предоставляет методы для получения комментариев.
+ * </p>
+ *
+ * @author Powered by ©AYE.team
+ * @version 0.0.1-SNAPSHOT
+ */
 @Repository
 public interface CommentRepository extends JpaRepository<CommentEntity, Integer> {
+    /**
+     * Получение всех комментариев объявления.
+     *
+     * @param id идентификатор объявления
+     * @return список комментариев {@link CommentEntity}
+     */
     List<CommentEntity> findAllByAdPk(int id);
 }
--- src/main/java/ru/skypro/homework/repository/ImageRepository.java
+++ src/main/java/ru/skypro/homework/repository/ImageRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.skypro.homework.entity.Image;
 
+/**
+ * Репозиторий для работы с таблицей IMAGES в базе данных.
+ *
+ * @author Powered by ©AYE.team
+ * @version 0.0.1-SNAPSHOT
+ */
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Integer> {
 }
