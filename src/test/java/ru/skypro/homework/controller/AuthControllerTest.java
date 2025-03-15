7683c3e34938e7ad76c928c0231c64491dfbef3e
goodmn79 <goodmn79@gmail.com>
2025-03-15 17:51:12 +0300

    optimized

--- src/test/java/ru/skypro/homework/controller/AuthControllerTest.java
+++ src/test/java/ru/skypro/homework/controller/AuthControllerTest.java
@@ -1,78 +1,101 @@
 package ru.skypro.homework.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.skypro.homework.config.WebSecurityConfig;
-import ru.skypro.homework.entity.UserEntity;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import ru.skypro.homework.dto.user.Login;
+import ru.skypro.homework.dto.user.Register;
 import ru.skypro.homework.service.AuthService;
-import ru.skypro.homework.service.impl.UserDetailServiceImpl;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(AuthController.class)
-@Import(WebSecurityConfig.class)
+@ExtendWith(MockitoExtension.class)
 class AuthControllerTest {
-    @Autowired
+
     private MockMvc mockMvc;
 
-    @MockBean
+    @Mock
     private AuthService authService;
 
-    @MockBean
-    private UserDetailServiceImpl userDetailService;
+    @InjectMocks
+    private AuthController authController;
 
-    private UserEntity userEntity;
+    private ObjectMapper objectMapper;
+    private String username;
+    private String password;
 
     @BeforeEach
     void setUp() {
-        userEntity = new UserEntity()
-                .setUsername("testUser")
-                .setPassword("password");
+        mockMvc = MockMvcBuilders.standaloneSetup(authController).build();
+
+        objectMapper = new ObjectMapper();
+        username = "email@example.com";
+        password = "password";
     }
 
     @Test
     void loginSuccess() throws Exception {
         when(authService.login(anyString(), anyString())).thenReturn(true);
 
-        mockMvc.perform(post("/login")
+        mockMvc
+                .perform(post("/login")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"username\":\"testUser\",\"password\":\"password\"}"))
+                        .content(
+                                objectMapper
+                                        .writeValueAsString(
+                                                new Login()
+                                                        .setUsername(username)
+                                                        .setPassword(password))
+                        ))
                 .andExpect(status().isOk());
 
-        verify(authService).login(userEntity.getUsername(), userEntity.getPassword());
+        verify(authService).login(eq(username), eq(password));
     }
 
     @Test
     void loginFailure() throws Exception {
         when(authService.login(anyString(), anyString())).thenReturn(false);
 
-        mockMvc.perform(post("/login")
+        mockMvc
+                .perform(post("/login")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"username\":\"testUser\",\"password\":\"password\"}"))
+                        .content(
+                                objectMapper
+                                        .writeValueAsString(
+                                                new Login()
+                                                        .setUsername(username)
+                                                        .setPassword(password))
+                        ))
                 .andExpect(status().isUnauthorized());
 
-        verify(authService).login(userEntity.getUsername(), userEntity.getPassword());
+        verify(authService).login(eq(username), eq(password));
     }
 
     @Test
     void registerSuccess() throws Exception {
         when(authService.register(any())).thenReturn(true);
 
-        mockMvc.perform(post("/register")
+        mockMvc
+                .perform(post("/register")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"username\":\"testUser\",\"password\":\"password\"}"))
+                        .content(
+                                objectMapper
+                                        .writeValueAsString(
+                                                new Register()
+                                                        .setUsername(username)
+                                                        .setPassword(password))
+                        ))
                 .andExpect(status().isCreated());
 
         verify(authService).register(any());
@@ -82,9 +105,16 @@ class AuthControllerTest {
     void registerFailure() throws Exception {
         when(authService.register(any())).thenReturn(false);
 
-        mockMvc.perform(post("/register")
+        mockMvc
+                .perform(post("/register")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"username\":\"testUser\",\"password\":\"password\"}"))
+                        .content(
+                                objectMapper
+                                        .writeValueAsString(
+                                                new Register()
+                                                        .setUsername(username)
+                                                        .setPassword(password))
+                        ))
                 .andExpect(status().isBadRequest());
 
         verify(authService).register(any());
